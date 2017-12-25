0e1d2b58b44a25fa4cd55be7d1af95d1af0737c2
alex <zhurbitsky@gmail.com>
2017-12-25 15:05:57 +0200

    Basic service worker using workbox-sw

--- project/src/sw.js
+++ project/src/sw.js
@@ -13,3 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+importScripts('workbox-sw.dev.v2.0.0.js');
+
+const workboxSW = new WorkboxSW();
+workboxSW.precache([]);
